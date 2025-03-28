e0183544e9f6a848f0f88d9b9abb8ded266e6912
Mourishitz <Gabrielmorishita@hotmail.com>
2025-03-28 15:26:47 -0300

    wip

--- src/app/Api/User/Repositories/UserRepository.ts
+++ src/app/Api/User/Repositories/UserRepository.ts
@@ -1,5 +1,7 @@
 import BaseRepository from "@Repositories/BaseRepository";
 import User from "@Api/User/Models/User";
+import database from "@/src/config/database";
+import Env from "@/src/config/env";
 
 class UserRepository extends BaseRepository<User> {
   constructor() {
